c7e07059cabf3e5278c1cb398539e0c471ec038d
Ashley Smith <ashesmsmith@gmail.com>
2025-11-06 15:56:51 -0700

    styling

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f146d77ab0c207b936fc7531a60fd4a8f98ae4df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c350ffd193d1fc6e80c2f21ace266549693f339")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
